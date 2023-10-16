31b91d57966e3a65e6a650e4add92b62dede0457
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-17 04:54:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,16 @@
 // var b = i[2]
 // console.log(b)
 // task 2
+// function name (){
+//     var newD = new Date ()
+//     document.write(newD)
+// } 
+// name()
+
+// task 3
 function name (){
-    var newD = new Date ()
-    document.write(newD)
-} 
+    var a = prompt("Write your first name")
+    var b = prompt("Write your last name")
+    var c = document.write(`${ a + b}`)
+}
 name()
